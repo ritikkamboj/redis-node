352af848ed097265e66bbd22736796db4dc11f19
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-06-15 09:54:22 +0530

    setting value to redis server and getting with the key

--- string.js
+++ string.js
@@ -1,7 +1,8 @@
 const client = require('./client')
 
 async function init() {
-    const result = await client.get("user:3");
+    await client.set("msg:6", "I am one of the Developer")
+    const result = await client.get("msg:6");
     console.log("results is ", result)
 }
 
