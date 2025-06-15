c608761ed7defa432532ce3cde69cc3bd5d9a9de
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-06-15 16:47:02 +0530

    use the redis to make our response faster and its works well

--- string.js
+++ string.js
@@ -1,7 +1,8 @@
 const client = require('./client')
 
 async function init() {
-    await client.set("msg:6", "I am one of the Developer")
+    // await client.set("msg:6", "I am one of the Developer")
+    await client.expire("msg:6", 10)
     const result = await client.get("msg:6");
     console.log("results is ", result)
 }
